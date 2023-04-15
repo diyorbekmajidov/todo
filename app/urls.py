bc9b0b1fe548e91443dd7df6c18764e741095f8b
Diyorbek <majidovdiyorbek741@gmail.com>
2023-04-15 13:59:44 +0500

    1

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from .views import CreateUserView, UserLogOut,UserLogIn,CreateTodo, TodoGetId, TodoUpdate, TodoDelete
+from django.urls import path
+
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create_user'),
+    path('logout/', UserLogOut.as_view(), name='logout'),
+    path("userlogin/", UserLogIn.as_view()),
+    path('createtodo/', CreateTodo.as_view()),
+    path('gettodo/<int:pk>/', TodoGetId.as_view()),
+    path('updatetodo/<int:pk>/', TodoUpdate.as_view()),
+    path('deletetodo/<int:pk>/', TodoDelete.as_view()),
+]

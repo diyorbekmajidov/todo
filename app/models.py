9b5322c299f99733ce599c5e6fcf9736bb4aef41
Diyorbek <majidovdiyorbek741@gmail.com>
2023-04-15 13:48:18 +0500

    1

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 # Create your models here.
@@ -8,7 +9,7 @@ class Todo(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     datecompleted = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
-    user = models.ForeignKey('auth.User', related_name='todos', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='todos', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
